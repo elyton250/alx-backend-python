d854655f86dc4672da4860d21d8c595312f51692
elyton250 <nelysemathieu@gmail.com>
2024-05-07 00:20:20 +0200

    anotehr  int

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,18 +1,11 @@
 #!/usr/bin/env python3
-"""This is the module that waits for some seconds"""
+"""this is an async module"""
 import asyncio
 import random
-from typing import Union
 
 
-async def wait_random(max_delay: int = 10) -> Union[int, float]:
-    """_summary_
-
-    Args:
-        max_delay (int, optional): _description_. Defaults to 10.
-
-    Returns:
-        Union[int, float]: _description_
-    """
-    await asyncio.sleep(max_delay)
-    return random.random() * max_delay 
+async def wait_random(max_delay: int = 10) -> float:
+    """this is the function that waits for a delay"""
+    delay: float = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
