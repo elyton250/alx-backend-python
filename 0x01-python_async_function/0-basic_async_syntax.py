cbaae74be356374ab671293582a99e15fd76d507
elyton250 <nelysemathieu@gmail.com>
2024-05-06 15:27:15 +0200

    hdihsfios

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,7 +6,13 @@ from typing import Union
 
 
 async def wait_random(max_delay: int = 10) -> Union[int, float]:
-    
-    
+    """_summary_
+
+    Args:
+        max_delay (int, optional): _description_. Defaults to 10.
+
+    Returns:
+        Union[int, float]: _description_
+    """
     await asyncio.sleep(max_delay)
     return random.random() * max_delay 
