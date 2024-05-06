af2721de96b9df10001d3f2e631d31a140c6fb63
elyton250 <nelysemathieu@gmail.com>
2024-05-07 00:51:23 +0200

    task 2

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -2,15 +2,16 @@
 """this for time measurement"""
 import asyncio
 import time
+from typing import Tuple
 
 
 wait_n = __import__("1-concurrent_coroutines").wait_n
 
 
-def measure_time(n, max_delay):
-    """this function is the for time meauremen"""
-    time_started = time.time()
+def measure_time(n: int, max_delay: int) -> float:
+    """this function is the for time measurement"""
+    time_started: float = time.time()
     asyncio.run(wait_n(n, max_delay))
-    time_ended = time.time()
-    elap_time = time_ended - time_started
+    time_ended: float = time.time()
+    elap_time: float = time_ended - time_started
     return elap_time / n
