dfa88228b1efdf896ea908c6a58eb0195cb7c5f9
elyton250 <nelysemathieu@gmail.com>
2024-05-07 11:10:19 +0200

    task 0

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""This module is for creating an asynchronous generator."""
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """This coroutine asynchronously yields random floats."""
+    n: int = 1
+    while n <= 10:
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
+        n += 1
